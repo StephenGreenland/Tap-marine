5cc5c3e78b6ac56cb1f6760cff61137e7387524f
Stephen Greenland <1013573@student.sae.edu.au>
2019-10-03 19:01:45 +0800

    Keyframe script

---
+++ DeepSeaclicker/Assets/timeframe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeframe : MonoBehaviour
+{
+    private bool startTimer;
+    private float timer;
+    private float startRecording;
+
+    public List<KeyFrame> KeyFrames;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 6f;
+        startTimer = false;
+        KeyFrames = new List<KeyFrame>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startTimer = true;
+            timer = 0f;
+        }
+
+        if (startTimer)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer < 5)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                KeyFrames.Add(new KeyFrame(timer));
+                Debug.Log(timer);
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            
+            KeyFrames.Clear();
+        }
+
+        if (Input.GetKeyDown((KeyCode.P)))
+        {
+            
+            
+        }
+    }
+    
+   
+}
+[Serializable]
+public class KeyFrame
+{
+    public KeyFrame(float time)
+    {
+        this.time = time;
+    }
+    public float time;
+}
