540afba5125a4124f4af889192457560dacde351
Stephen Greenland <1013573@student.sae.edu.au>
2019-10-03 12:42:35 +0800

    Made unity project

---
+++ DeepSeaclicker/Assets/CameraScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraScript : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, mousePos2D);
+            if (hit.collider != null)
+            {
+                
+                hit.transform.position = new Vector2(Random.Range(-5,5),Random.Range(-5,5));
+            }
+        }
+
+
+    }
+}
