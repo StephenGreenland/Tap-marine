04fa454221cfb2822450b9e6e3cbc9e933495c43
bryce <1011998@student.sae.edu.au>
2019-10-07 08:15:49 +0800

    Gold, hp, damage scripts added

---
+++ DeepSeaclicker/Assets/Scripts/Damage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public UnityEngine.UI.Text DamageDisplay;
+    public float StaticDamage=0;
+    public float Damagedealt = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StaticDamage += Damagedealt;
+            //   gameObject.GetComponent<Healthscript>().TakeDamage(Damagedealt); //enemy health
+            DamageDisplay.text = "Damage:" + StaticDamage;    //interact with damage text display
+                        
+        }
+        if (Input.GetKeyUp(KeyCode.Space))    // reset damage
+        {
+            StaticDamage = 0;
+            DamageDisplay.text = "Damage:" + StaticDamage;
+        }
+    }
+}
---
+++ DeepSeaclicker/Assets/Scripts/Healthscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthscript : MonoBehaviour
+{
+    public bool dead;
+    public float MaxHealth=10f;
+    public float CurrentHealth = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+        dead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float amount)
+    {
+        if (CurrentHealth<= 0){
+            CurrentHealth = 0;
+            dead = true;
+            //destroy and load next enemy
+        }
+        if (!dead)
+        {
+            return;
+        }
+
+        CurrentHealth -= amount;
+    }
+}
