ad9e6f4a6251ea53da793980958124ae2d992431
Stephen Greenland <1013573@student.sae.edu.au>
2019-10-17 14:02:12 +0800

    push for cam

--- DeepSeaclicker/Assets/Scripts/ClickedManager.cs
+++ DeepSeaclicker/Assets/Scripts/ClickedManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickManager : MonoBehaviour
+public class ClickedManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -11,20 +11,26 @@ public class ClickManager : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10f));
+           
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null)
-            {
-                
-                hit.transform.position = new Vector2(Random.Range(-5,5),Random.Range(-5,5));
-            }
+            RaycastHit2D hit = Physics2D.Raycast(mousePosWorld, Vector2.zero);
+           
+             if (hit.collider != null)
+              {
+                  
+                  hit.transform.position = new Vector2(Random.Range(-5,5),Random.Range(-5,5));
+              } 
         }
     }
 }
+
+
