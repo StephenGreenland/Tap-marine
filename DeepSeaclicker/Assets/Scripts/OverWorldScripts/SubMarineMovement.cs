63dfff54c8cb0a1e617a64e0e5c81648061e4890
Rashid Dennaoui <1003877@student.sae.edu.au>
2019-10-17 15:37:12 +1100

    Overworld Work - Rashid

---
+++ DeepSeaclicker/Assets/Scripts/OverWorldScripts/SubMarineMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubMarineMovement : MonoBehaviour
+{
+	private Vector2 targetPos;
+	private Vector2 currentPos;
+
+	public float speed;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		currentPos = this.transform.position;
+		targetPos =  transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+		targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+
+		Debug.Log(targetPos);
+
+		if (Input.GetMouseButton(0))
+		{
+			Debug.Log("CLICKED");
+
+			transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+			//this.transform.LookAt(targetPos);
+		}
+    }
+}
