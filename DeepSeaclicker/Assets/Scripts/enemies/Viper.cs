52b71e56f5e56c0fa56368f4ba207237c38abc8c
bryce <1011998@student.sae.edu.au>
2019-10-24 20:12:57 +1100

    Auto stash before merge of "master" and "origin/master"

---
+++ DeepSeaclicker/Assets/Scripts/enemies/Viper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viper : MonsterBase
+{
+    public Health health;
+
+    private void OnEnable()
+    {
+        health.OnChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        health.OnChanged -= OnHealthChanged;
+    }
+
+    private void OnHealthChanged(float amount)
+    {
+
+        if (health.amount <= 0)
+        {
+            OnLeave();
+            Destroy(gameObject);
+
+        }
+    }
+}
