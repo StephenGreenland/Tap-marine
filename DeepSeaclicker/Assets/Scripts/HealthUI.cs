bca3d3d39694705a1bd5fb7e0766ce81543bb21c
Kartik <1013041@student.sae.edu.au>
2019-10-25 10:09:44 +0800

    Added battle scene UI

---
+++ DeepSeaclicker/Assets/Scripts/HealthUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public Text textRef;
+    public GameObject enemyRef;
+    public float amountRef;
+    // Update is called once per frame
+    
+    void Update()
+    {
+        
+        if (enemyRef == null)
+        {
+            enemyRef = GameObject.FindGameObjectWithTag("Enemy");
+        }
+        if (enemyRef != null)
+        {
+            amountRef = enemyRef.GetComponent<Health>().amount;
+            textRef.text = "Enemy Health: " + amountRef;
+            Debug.Log(textRef.text);
+        }
+    }
+}
