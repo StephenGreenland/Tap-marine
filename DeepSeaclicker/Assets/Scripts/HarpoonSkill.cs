843ca205e350492be46e26318ea8dd49a2a7ef2e
Kartik <1013041@student.sae.edu.au>
2019-11-15 10:31:01 +0800

    UI buttons and harpoon skill script

---
+++ DeepSeaclicker/Assets/Scripts/HarpoonSkill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarpoonSkill : MonoBehaviour
+{
+    bool skillActive = false;
+    public float HarpoonDamage;
+    public float cooldownTime;
+    public GameObject clickManagerRef;
+
+    public void HarpoonActivated()
+    {
+        if (skillActive) return;
+        clickManagerRef.GetComponent<ClickedManager>().damage -= HarpoonDamage;
+    }
+    public void Update()
+    {
+        Debug.Log(clickManagerRef.GetComponent <ClickedManager> ().damage);
+    }
+}
