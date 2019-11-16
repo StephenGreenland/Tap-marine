94001105d1f172683e09bc8026b2d016ec05f45e
Kyan <rowse9@gmail.com>
2019-11-16 13:31:21 +1100

    Added Audio Script onto a empty game object on OverWorld & Battlescene.

---
+++ DeepSeaclicker/Assets/Scripts/BattleSceneAudio.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneAudio : MonoBehaviour
+{
+    public MonsterManager MonsterManager;
+    FMOD.Studio.EventInstance BattleTrack1;
+    private void Start()
+    {    
+        MonsterManager.Onleave += MonsterManager_Onleave;
+        MonsterManager.OnNew += MonsterManager_OnNew;
+
+        BattleTrack1 = FMODUnity.RuntimeManager.CreateInstance("event:/Music/BattleMusic");
+        BattleTrack1.start();
+    }
+
+    //Monster Spawn Sound
+    private void MonsterManager_OnNew(MonsterBase obj)
+    {
+
+        //obj.GetComponent<Health>().OnChanged += BattleSceneAudio_OnChanged
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Enemies/MonsterSpawn", gameObject);
+    }
+
+    //Monster Death Sound
+    private void MonsterManager_Onleave(MonsterBase obj)
+    {
+   
+    }
+
+    private void OnDestroy()
+    {
+        MonsterManager.Onleave -= MonsterManager_Onleave; 
+        MonsterManager.OnNew -= MonsterManager_OnNew; 
+    }
+    void Update()
+    {
+        //Main Cannon
+        if (Input.GetMouseButtonDown(0))
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Player/Weapons/Cannon", gameObject);
+        }
+
+        //Harpoon TEST
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Player/Weapons/Harpoon", gameObject);
+        }
+        
+        //Torpedo TEST
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Player/Weapons/Torpedo", gameObject);
+        }
+    }
+}
