352b3b339b4e62da14185c6c97c6f551f04f02c1
bryce <1011998@student.sae.edu.au>
2019-11-21 19:17:00 +1100

    All regular enemies implemented

---
+++ DeepSeaclicker/Assets/Scripts/enemies/ghostship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostship : MonsterBase
+{
+
+    public Health health;
+
+    private void OnEnable()
+    {
+        health.OnChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("DIE!");
+        health.OnChanged -= OnHealthChanged;
+    }
+
+    private void OnHealthChanged(float amount)
+    {
+
+        if (health.amount <= 0)
+        {
+            OnLeave();
+            Destroy(gameObject);
+
+        }
+    }
+}
