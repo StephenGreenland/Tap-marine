6c949daf45fc32543b11f51ebfadbf82cb418ad5
bryce <1011998@student.sae.edu.au>
2019-11-21 17:44:22 +1100

    prefabs

---
+++ DeepSeaclicker/Assets/Scripts/enemies/bosses/bugscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bugscript : MonsterBase
+{
+    public Health health;
+    public string sceneToLoad;
+    private void OnEnable()
+    {
+        health.OnChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        health.OnChanged -= OnHealthChanged;
+    }
+
+    private void OnHealthChanged(float amount)
+    {
+
+        if (health.amount <= 0)
+        {
+            OnLeave();
+            Destroy(gameObject);
+            SceneManager.LoadScene(sceneToLoad);
+
+        }
+    }
+}
