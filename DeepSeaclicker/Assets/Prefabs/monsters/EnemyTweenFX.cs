1a570819913043a2f718896b03db6705af82cb05
Rashid Dennaoui <1003877@student.sae.edu.au>
2019-11-29 09:32:08 +0800

    Tweening FX

---
+++ DeepSeaclicker/Assets/Prefabs/monsters/EnemyTweenFX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+
+public class EnemyTweenFX : MonoBehaviour
+{
+
+	public Health health;
+	public ClickedManager clickManager;
+	public Renderer rend;
+	
+	private void OnEnable()
+	{
+		clickManager = GameObject.FindGameObjectWithTag("ClickManager").GetComponent<ClickedManager>();
+		health.OnChanged += OnHealthChanged;
+		clickManager.onCrit += OnCritical;
+	}
+
+	private void OnDisable()
+	{
+		health.OnChanged -= OnHealthChanged;
+	}
+
+	private void OnHealthChanged(float amount)
+	{
+		transform.DOPunchScale(new Vector3 (0.05f,0.05f,0.05f), 0.05f, 10, 2f);
+	}
+
+	public void OnCritical()
+	{
+
+		rend.material.color = new Color(1f, 0.5f, 0.5f); rend.material.DOColor(Color.white, 0.5f);
+		transform.DOPunchScale(new Vector3(0.15f, 0.15f, 0.15f), 0.7f, 10, 2f);
+	}
+}
