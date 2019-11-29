898bc12423b9aff04463ec9d85a46e4424ef2dd9
Rashid Dennaoui <1003877@student.sae.edu.au>
2019-11-29 09:56:29 +0800

    Tweening FX Complete

--- DeepSeaclicker/Assets/Scripts/Tweening/EnemyTweenFX.cs
+++ DeepSeaclicker/Assets/Scripts/Tweening/EnemyTweenFX.cs
@@ -12,6 +12,7 @@ public class EnemyTweenFX : MonoBehaviour
 	public ClickedManager clickManager;
 	public Renderer rend;
 	
+
 	private void OnEnable()
 	{
 		clickManager = GameObject.FindGameObjectWithTag("ClickManager").GetComponent<ClickedManager>();
@@ -22,17 +23,21 @@ public class EnemyTweenFX : MonoBehaviour
 	private void OnDisable()
 	{
 		health.OnChanged -= OnHealthChanged;
+		clickManager.onCrit -= OnCritical;
 	}
 
 	private void OnHealthChanged(float amount)
 	{
-		transform.DOPunchScale(new Vector3 (0.05f,0.05f,0.05f), 0.05f, 10, 2f);
+		transform.DOPunchScale(new Vector3 (0.05f,0.05f,0.05f), 0.1f, 10, 2f);
 	}
 
 	public void OnCritical()
 	{
 
 		rend.material.color = new Color(1f, 0.5f, 0.5f); rend.material.DOColor(Color.white, 0.5f);
-		transform.DOPunchScale(new Vector3(0.15f, 0.15f, 0.15f), 0.7f, 10, 2f);
+
+		transform.DOShakePosition(0.3f, 1f, 10, 90, false, true);
+
+		//transform.DOPunchScale(new Vector3(0.1f, 0.11f, 0.1f), 0.05f, 10, 2f);
 	}
 }
