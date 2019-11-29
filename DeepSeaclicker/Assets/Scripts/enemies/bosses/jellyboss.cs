fd96492bf2ae9ff4b60f4174380898fd7bb9c447
bryce <1011998@student.sae.edu.au>
2019-11-29 12:23:13 +1100

    level saving

--- DeepSeaclicker/Assets/Scripts/enemies/bosses/jellyboss.cs
+++ DeepSeaclicker/Assets/Scripts/enemies/bosses/jellyboss.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 public class jellyboss : MonsterBase
 
 {
+    public MonsterManager fixenemy;
     FMOD.Studio.Bus MasterBus;
     public Health health;
     public string sceneToLoad;
+    public Scenemanager scenemanager;
     private void OnEnable()
     
     
@@ -28,11 +30,13 @@ public class jellyboss : MonsterBase
 
         if (health.amount <= 0)
         {
-            OnLeave();
             Destroy(gameObject);
-            SceneManager.LoadScene(sceneToLoad);
+            OnLeave();
+            scenemanager.SaveGame();
+           
+         
             MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-
+            fixenemy.monsterLevel = 0;
         }
     }
 }
